480621467f47f9faf1c9b34bee4b873a9eaf7b06
raknair <rakesh.nair@accelya.com>
2021-04-10 12:51:12 +0530

    Added security expression to validate

--- src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/controller/UsersController.java
+++ src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/controller/UsersController.java
@@ -2,6 +2,8 @@ package com.appsdeveloperblog.ws.api.ResourceServer.controller;
 
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,9 +15,9 @@ public class UsersController {
     }
 
     //@Secured("ROLE_developer")
-    @PreAuthorize("hasAuthority('ROLE_developer')")
+    @PreAuthorize("hasAuthority('ROLE_developer') or #id == #jwt.subject")
     @DeleteMapping(path="/{id}")
-    public String deleteUser(@PathVariable String id){
-        return "Deleted User with Id "+id;
+    public String deleteUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt){
+        return "Deleted User with Id "+id+" and Jwt Subject "+jwt.getSubject();
     }
 }
